5d76961c8633dc28aa49992ef273ae0dcc028e69
YWTechIT <abcd8637@naver.com>
2021-08-11 09:18:36 +0900

    ADD intersectionObserver
    1. 렌더링 이후 useEffect를 한번 실행하여 ` getPosts() ` 함수를 실행한다. (getPosts()함수는 api를 받아오는 함수임)
    2. 현재 `scroll`이 마지막까지 내려오면 `useRef`를 바라보게 되고 ` intersection `이 실행되면서 ` getPosts() `를 실행하고  다음 `data`를 불러온다.
    3. 불러온 `data`를 `setData`에 추가한다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import "./App.css";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Comments } from "./types";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState<Comments[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error>();
+  const [currentPostId, setCurrentPostId] = useState(1);
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
+  const getPosts = useCallback(
+    async (currentPostId: number) => {
+      try {
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/comments?postId=${currentPostId}`
+        );
+
+        setData([...data, ...response.data]);
+        setLoading(false);
+      } catch (e) {
+        setError(e);
+      }
+    },
+    [data]
+  );
+
+  useEffect(() => {
+    getPosts(currentPostId);
+  }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    const io = new IntersectionObserver(function (entries) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && loadMoreRef.current) {
+          setCurrentPostId((currentPostId) => currentPostId + 1);
+          getPosts(currentPostId);
+        }
+      });
+    });
+
+    const el = loadMoreRef && loadMoreRef.current;
+
+    if (!el) {
+      return;
+    }
+
+    io.observe(el);
+
+    return () => {
+      io.unobserve(el);
+      setLoading(false);
+    };
+  }, [getPosts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data &&
+        data.map((item) => (
+          <div className="wrapper" key={item.id}>
+            <section className="card">
+              <div>
+                {item.id}, {item.body}
+              </div>
+            </section>
+          </div>
+        ))}
+      <div ref={loadMoreRef}></div>
+      {loading && <div>loading...</div>}
     </div>
   );
-}
+};
 
 export default App;
